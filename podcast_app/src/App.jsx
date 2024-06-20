ad1f2329e1b6405bf32130073405a8c3c4073dac
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-06-20 13:37:22 +0200

    added code to App.jsx and to main.jsx

--- podcast_app/src/App.jsx
+++ podcast_app/src/App.jsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// routes
+import {
+  Home,
+  Signup,
+  SinglePodcast,
+  Login,
+  Favourites,
+  PageNotFound,
+} from './pages';
+import supabase from './supabase/client';
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  setAllPodcasts,
+  setIsLoading,
+  setHomePageDisplayedPodcasts,
+  setUserDataFromDB,
+  setHasAccount,
+  setFavourites,
+  setSortSearchFavouritesArray,
+} from './globalState/reducers/podcastsReducer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { favourites, favouriteSwitch } = useSelector(
+    (state) => state.podcastsReducer
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setIsLoading(true));
+    const getPodcasts = async () => {
+      const response = await fetch('https://podcast-api.netlify.app/shows');
+      const result = await response.json();
+
+      if (result) {
+        dispatch(setHomePageDisplayedPodcasts(result));
+        dispatch(setAllPodcasts(result));
+
+        dispatch(setIsLoading(false));
+      } else {
+        console.log('error');
+      }
+      return result;
+    };
+    getPodcasts();
+  }, []);
+
+  const fetchLoginData = async () => {
+    const { data, error } = await supabase.from('user_login_data').select();
+    if (error) {
+      console.log(error);
+    }
+    if (data.length !== 0) {
+      dispatch(setUserDataFromDB(data));
+      dispatch(setHasAccount(true));
+    }
+  };
+  useEffect(() => {
+    fetchLoginData();
+  }, []);
+
+  const fetchFavouritesFromDB = async () => {
+    const { data, error } = await supabase.from('userFavourites').select();
+    if (data) {
+      dispatch(setFavourites(data));
+    }
+  };
+  useEffect(() => {
+    fetchFavouritesFromDB();
+  }, [favouriteSwitch]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/podcast/:id" element={<SinglePodcast />} />
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
